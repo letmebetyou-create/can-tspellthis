9878e5fe582d3b97b225c24cabcb6a8e11c53d57
letmebetyou-create <letmebetyou@gmail.com>
2026-07-20 22:38:32 +0800

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,50 @@
+// =================================
+// LOCAL STORAGE
+// =================================
 
+
+function saveUser(user) {
+
+
+    localStorage.setItem(
+        "betYouUser",
+        JSON.stringify(user)
+    );
+
+
+}
+
+
+
+function getSavedUser() {
+
+
+    const saved =
+        localStorage.getItem(
+            "betYouUser"
+        );
+
+
+    if (!saved) {
+
+        return null;
+
+    }
+
+
+    return JSON.parse(saved);
+
+
+}
+
+
+
+function clearUser() {
+
+
+    localStorage.removeItem(
+        "betYouUser"
+    );
+
+
+}
