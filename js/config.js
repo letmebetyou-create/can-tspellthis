8f3ae0e741e551cc8985acb3fe98bd73e5389ed1
letmebetyou-create <letmebetyou@gmail.com>
2026-07-20 22:38:06 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,14 @@
+// =================================
+// CONFIGURATION
+// =================================
 
+const CONFIG = {
+
+    APP_NAME:
+        "BET YOU CAN'T SPELL THIS",
+
+
+    API_URL:
+        ""
+
+};
