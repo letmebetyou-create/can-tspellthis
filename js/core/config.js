e0bf8aceea7fbedf722d3a4306b438ba26a20b66
letmebetyou-create <letmebetyou@gmail.com>
2026-07-22 00:01:47 +0800

    Update config.js

--- js/core/config.js
+++ js/core/config.js
@@ -1,1 +1,42 @@
+// =================================
+// BET YOU CAN'T SPELL THIS
+// Frontend Configuration
+// =================================
 
+const CONFIG = {
+
+    // =================================
+    // Application
+    // =================================
+
+    startPage: "home",
+
+    // =================================
+    // Themes
+    // =================================
+
+    activeTheme: APP.defaultTheme,
+
+    // =================================
+    // Language
+    // =================================
+
+    language: APP.defaultLanguage,
+
+    // =================================
+    // Storage
+    // =================================
+
+    usernameStorageKey: "bst_username",
+
+    recoveryCodeStorageKey: "bst_recovery_code",
+
+    themeStorageKey: "bst_theme",
+
+    // =================================
+    // Notifications
+    // =================================
+
+    notificationsPerPage: 1
+
+};
