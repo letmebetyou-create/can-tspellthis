88cae33f8f168960d6c1ec7d001fe4ca6eedd145
letmebetyou-create <letmebetyou@gmail.com>
2026-07-22 00:07:33 +0800

    Update state.js

--- js/core/state.js
+++ js/core/state.js
@@ -1,1 +1,64 @@
+// =================================
+// BET YOU CAN'T SPELL THIS
+// Application State
+// =================================
 
+const STATE = {
+
+    // =================================
+    // Application
+    // =================================
+
+    CurrentPage: null,
+
+    IsLoading: false,
+
+    // =================================
+    // User
+    // =================================
+
+    Username: null,
+
+    RecoveryCode: null,
+
+    // =================================
+    // Notifications
+    // =================================
+
+    Notifications: [],
+
+    CurrentNotification: 0,
+
+    // =================================
+    // Daily Challenge
+    // =================================
+
+    CurrentChallenge: null,
+
+    CurrentWordIndex: 0,
+
+    PlayerAnswers: [],
+
+    // =================================
+    // Leaderboard
+    // =================================
+
+    Leaderboard: [],
+
+    // =================================
+    // Archive
+    // =================================
+
+    Archive: [],
+
+    // =================================
+    // Practice Blitz
+    // =================================
+
+    BlitzWords: [],
+
+    BlitzWordIndex: 0,
+
+    BlitzAnswers: []
+
+};
