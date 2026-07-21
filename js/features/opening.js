505d06ed0286347aa04222e264d8565f66a545cd
letmebetyou-create <letmebetyou@gmail.com>
2026-07-22 00:55:14 +0800

    Update opening.js

--- js/features/opening.js
+++ js/features/opening.js
@@ -1,1 +1,60 @@
+// =================================
+// BET YOU CAN'T SPELL THIS
+// Opening Screen
+// =================================
 
+
+function showOpening() {
+
+
+    app.innerHTML = `
+
+        <div class="opening-screen">
+
+
+            <div class="opening-title">
+
+                BET YOU CAN'T SPELL THIS
+
+            </div>
+
+
+            <div class="opening-line">
+
+                ========================================
+
+            </div>
+
+
+            <div class="bee-container">
+
+                🐝
+
+            </div>
+
+
+        </div>
+
+    `;
+
+
+    setTimeout(function(){
+
+        goToHome();
+
+    }, 2500);
+
+
+}
+
+
+
+// =================================
+// Opening Navigation
+// =================================
+
+function goToHome(){
+
+    loadPage("home");
+
+}
