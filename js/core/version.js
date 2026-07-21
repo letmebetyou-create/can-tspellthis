355acef6fac229624f415c6c295c63e5ad49c370
letmebetyou-create <letmebetyou@gmail.com>
2026-07-22 00:01:05 +0800

    Update version.js

--- js/core/version.js
+++ js/core/version.js
@@ -1,1 +1,26 @@
+// =================================
+// BET YOU CAN'T SPELL THIS
+// Version Information
+// =================================
 
+const APP = {
+
+    // Application
+
+    name: "Bet You Can't Spell This",
+
+    repository: "Can't Spell This",
+
+    // Version
+
+    version: "v0.1.0",
+
+    // Theme
+
+    defaultTheme: "default",
+
+    // Language
+
+    defaultLanguage: "en-GB"
+
+};
