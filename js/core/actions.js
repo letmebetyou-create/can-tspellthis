73935ec46a16769a796d13bac9b9b71845815f8b
letmebetyou-create <letmebetyou@gmail.com>
2026-07-22 00:14:06 +0800

    Create actions.js

---
+++ js/core/actions.js
@@ -0,0 +1,58 @@
+// =================================
+// BET YOU CAN'T SPELL THIS
+// Backend Actions
+// =================================
+
+const ACTIONS = {
+
+    // =================================
+    // Settings
+    // =================================
+
+    LoadSettings: "loadSettings",
+
+    // =================================
+    // User
+    // =================================
+
+    LoginUser: "loginUser",
+
+    CreateUser: "createUser",
+
+    RecoverUser: "recoverUser",
+
+    // =================================
+    // Notifications
+    // =================================
+
+    LoadNotifications: "loadNotifications",
+
+    // =================================
+    // Daily Challenge
+    // =================================
+
+    LoadDailyChallenge: "loadDailyChallenge",
+
+    SubmitChallenge: "submitChallenge",
+
+    // =================================
+    // Archive
+    // =================================
+
+    LoadArchive: "loadArchive",
+
+    // =================================
+    // Leaderboard
+    // =================================
+
+    LoadLeaderboard: "loadLeaderboard",
+
+    // =================================
+    // Suggestions
+    // =================================
+
+    LoadSuggestions: "loadSuggestions",
+
+    SubmitSuggestion: "submitSuggestion"
+
+};
