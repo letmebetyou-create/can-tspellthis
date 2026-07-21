44abab0eb0de2f3f839c8e179ba537a4f0495de5
letmebetyou-create <letmebetyou@gmail.com>
2026-07-22 00:17:18 +0800

    Update app.js

--- js/core/app.js
+++ js/core/app.js
@@ -1,1 +1,46 @@
+// =================================
+// BET YOU CAN'T SPELL THIS
+// Application
+// =================================
 
+
+// =================================
+// Start Application
+// =================================
+
+async function startApp() {
+
+    console.log("=================================");
+    console.log(APP.name);
+    console.log(APP.version);
+    console.log("=================================");
+
+    // Load settings
+
+    await loadSettings();
+
+    // Load first page
+
+    await loadPage(
+        SETTINGS.StartPage
+    );
+
+}
+
+
+
+// =================================
+// Application Ready
+// =================================
+
+document.addEventListener(
+
+    "DOMContentLoaded",
+
+    function () {
+
+        startApp();
+
+    }
+
+);
