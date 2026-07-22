d83d49f03946d6e0b7998d76a5189dd951a47bc9
letmebetyou-create <letmebetyou@gmail.com>
2026-07-22 11:52:56 +0800

    Update footer.js

--- js/components/footer.js
+++ js/components/footer.js
@@ -1,1 +1,32 @@
+// =================================
+// BET YOU CAN'T SPELL THIS
+// Footer
+// =================================
 
+
+// =================================
+// Initialise Footer
+// =================================
+
+function initFooter() {
+
+    const footer =
+        document.getElementById("footer");
+
+    if (!footer) {
+
+        return;
+
+    }
+
+    footer.innerHTML = `
+
+        <p class="version">
+
+            ${APP.version}
+
+        </p>
+
+    `;
+
+}
