80aa49c4639bca0b208b34a7effbe6e50e519b1d
letmebetyou-create <letmebetyou@gmail.com>
2026-07-22 11:46:21 +0800

    Update app.js

--- js/core/app.js
+++ js/core/app.js
@@ -10,7 +10,18 @@
 
 function initialiseApp() {
 
+
+    // Restore local user
+    // (Coming Soon)
+
+
+    // Load settings
+    // (Coming Soon)
+
+
+    // Decide starting page
     loadPage("opening");
+
 
 }
 
